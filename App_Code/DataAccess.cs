7fd2ec548b892d6e052990b2ea6a82c607852805
r0flbear <mathias.hansen783@gmail.com>
2014-09-09 12:52:48 +0200

    misc files added

---
+++ App_Code/DataAccess.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+public class DataAccess
+{
+        SqlConnection _Con = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);  
+
+        public DataTable GetData(SqlCommand CMD) 
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter objDa = new SqlDataAdapter();
+
+            try
+            {
+                CMD.Connection = _Con;
+                objDa.SelectCommand = CMD;
+                objDa.Fill(dt);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return dt;
+        }
+        public void ModifyData(SqlCommand CMD)
+        {
+            CMD.Connection = _Con;
+            _Con.Open();
+            CMD.ExecuteNonQuery();
+            _Con.Close();
+        }
+}
---
+++ App_Code/forside.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+public class forside
+{
+    DataAccess da = new DataAccess();
+    SqlCommand cmd = new SqlCommand();
+    DataTable dt = new DataTable();
+
+    public DataTable getForsideContent() {
+        cmd.CommandText = "SELECT [sm_forside].[header], [sm_content].[text], [sm_content].[image], [sm_content].[side], [sm_content].[forside_fk], [sm_content].[img_width], [sm_content].[img_height] FROM [sm_content] INNER JOIN [sm_forside] ON [sm_content].[forside_fk] = [sm_forside].[id]";
+        return dt = da.GetData(cmd);
+    }
+}
